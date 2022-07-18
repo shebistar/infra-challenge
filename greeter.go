8d234e2bb418f64a3aed4549f5e1e5b64c8d1d7d
Shebistar <sebastian_bullshit@hotmail.com>
2022-07-18 20:48:25 +0200

    Add HELLO_TAG to go web response

--- greeter.go
+++ greeter.go
@@ -14,7 +14,7 @@ func main() {
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmtStr := fmt.Sprintf("Hello, %s! I'm %s", GetIPFromRequest(r), os.Getenv("HOSTNAME"))
+	fmtStr := fmt.Sprintf("Hello, %s! I'm %s", GetIPFromRequest(r), os.Getenv("HOSTNAME"), os.Getenv("HELLO_TAG")))
 	fmt.Println(fmtStr)
 	fmt.Fprintln(w, fmtStr)
 }
