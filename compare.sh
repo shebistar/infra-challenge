0a3bfe0708e5cf6d8172f2f590937d41e92bde35
Shebistar <sebastian_bullshit@hotmail.com>
2022-08-16 08:48:00 +0200

    delete compare.sh script

--- compare.sh
+++
@@ -1,7 +0,0 @@
-while [ "$ELB" != "null" ] 
-do
-echo "waiting on ELB to come up......."
-ELB=$(kubectl get service hivemind-app -n hivemind -o json | jq -r '.status.loadBalancer.ingress[].hostnam')
-sleep 10
-done
-
